76bc428c5d3e9a3732df60b563177591b4058570
Amit Bhatt <amitbhatt54341@gmail.com>
2026-02-14 17:07:29 +0530

    Update API URL to Azure backend

--- BitsNBytes/src/environments/environment.ts
+++ BitsNBytes/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://orange-water-06cf16100.6.azurestaticapps.net',
+  apiBaseUrl: 'https://bitsbytesapi20260212185725-b3fad4amfac3gdd0.centralindia-01.azurewebsites.net',
   environmentName: 'production'
 };
