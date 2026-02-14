fc1553e2d12dd21277cc9966420273f7a1963533
Amit Bhatt <amitbhatt54341@gmail.com>
2026-02-14 17:46:59 +0530

    added debugging message

--- BitsNBytes/src/app/app.ts
+++ BitsNBytes/src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./core/components/navbar/navbar";
 import { ImageSelector } from './shared/components/image-selector/image-selector';
 import { AuthService } from './features/auth/services/auth-service';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,11 @@ import { AuthService } from './features/auth/services/auth-service';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+  ngOnInit() {
+    console.log('API URL:', environment.apiBaseUrl);
+    console.log('Production mode:', environment.production);
+  }
   protected readonly title = signal('BitsNBytes');
   authService = inject(AuthService);
   loadUserRef = this.authService.loadUser();
